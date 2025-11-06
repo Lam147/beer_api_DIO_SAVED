46d9df233a98b6437e90300c5fc7d0bdc348ca8d
Léo Melo 147 <lahmjj@gmail.com>
2025-11-05 21:54:38 -0300

    fix: restaura BeerService com deleteById(id) - versão funcional

--- src/main/java/one/digitalinnovation/beerstock/service/BeerService.java
+++ src/main/java/one/digitalinnovation/beerstock/service/BeerService.java
@@ -8,7 +8,93 @@ import one.digitalinnovation.beerstock.exception.BeerNotFoundException;
 import one.digitalinnovation.beerstock.exception.BeerStockExceededException;
 import one.digitalinnovation.beerstock.mapper.BeerMapper;
 import one.digitalinnovation.beerstock.repository.BeerRepository;
+import org.springframework.beans.package one.digitalinnovation.beerstock.service;
+
+import one.digitalinnovation.beerstock.dto.BeerDTO;
+import one.digitalinnovation.beerstock.dto.QuantityDTO;
+import one.digitalinnovation.beerstock.entity.Beer;
+import one.digitalinnovation.beerstock.exception.BeerAlreadyRegisteredException;
+import one.digitalinnovation.beerstock.exception.BeerNotFoundException;
+import one.digitalinnovation.beerstock.exception.BeerStockExceededException;
+import one.digitalinnovation.beerstock.mapper.BeerMapper;
+import one.digitalinnovation.beerstock.repository.BeerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class BeerService {
+
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper = BeerMapper.INSTANCE;
+
+    @Autowired
+    public BeerService(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    public BeerDTO createBeer(BeerDTO beerDTO) throws BeerAlreadyRegisteredException {
+        verifyIfIsAlreadyRegistered(beerDTO.getName());
+        Beer beer = beerMapper.toModel(beerDTO);
+        Beer savedBeer = beerRepository.save(beer);
+        return beerMapper.toDTO(savedBeer);
+    }
+
+    public BeerDTO findByName(String name) throws BeerNotFoundException {
+        Beer foundBeer = beerRepository.findByName(name)
+                .orElseThrow(() -> new BeerNotFoundException(name));
+        return beerMapper.toDTO(foundBeer);
+    }
+
+    public List<BeerDTO> listAll() {
+        return beerRepository.findAll()
+                .stream()
+                .map(beerMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public void deleteById(Long id) throws BeerNotFoundException {
+        verifyIfExists(id);
+        beerRepository.deleteById(id);
+    }
+
+    public BeerDTO increment(Long id, int quantityToIncrement) throws BeerNotFoundException, BeerStockExceededException {
+        Beer beer = verifyIfExists(id);
+        int newQuantity = beer.getQuantity() + quantityToIncrement;
+        if (newQuantity > beer.getMax()) {
+            throw new BeerStockExceededException(id, quantityToIncrement);
+        }
+        beer.setQuantity(newQuantity);
+        Beer updatedBeer = beerRepository.save(beer);
+        return beerMapper.toDTO(updatedBeer);
+    }
+
+    public BeerDTO decrement(Long id, int quantityToDecrement) throws BeerNotFoundException, BeerStockExceededException {
+        Beer beer = verifyIfExists(id);
+        int newQuantity = beer.getQuantity() - quantityToDecrement;
+        if (newQuantity < 0) {
+            throw new BeerStockExceededException(id, quantityToDecrement);
+        }
+        beer.setQuantity(newQuantity);
+        Beer updatedBeer = beerRepository.save(beer);
+        return beerMapper.toDTO(updatedBeer);
+    }
+
+    private Beer verifyIfExists(Long id) throws BeerNotFoundException {
+        return beerRepository.findById(id)
+                .orElseThrow(() -> new BeerNotFoundException(id));
+    }
+
+    private void verifyIfIsAlreadyRegistered(String name) throws BeerAlreadyRegisteredException {
+        Optional<Beer> optSavedBeer = beerRepository.findByName(name);
+        if (optSavedBeer.isPresent()) {
+            throw new BeerAlreadyRegisteredException(name);
+        }
+    }
+}.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
